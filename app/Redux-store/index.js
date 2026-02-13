a6f28299b6c3444d362fe09ccc9a84819a2cdbb5
Aftab Ahmad/Professional Services/Lahore <Aftab.Ahmad@Wateen.com>
2026-02-13 12:43:03 +0500

    remove delete files/folders

--- app/Redux-store/index.js
+++ app/Redux-store/index.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import authReducer from "./slices/authSlice";
-import uiReducer from "./slices/uiSlice";
 import { baseApi } from "./services/baseApi";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    ui: uiReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
